abf07159acf1957cbaf9638575255d61b4e4ce5d
Soumyaranjan Kundu <kundusoumyaranjan480@gmail.com>
2026-09-18 09:38:53 +0200

    Prepare project for Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yaadvibrator.billing',
+  appName: 'Yaad Vibrator',
+  webDir: '.output/public'
+};
+
+export default config;
